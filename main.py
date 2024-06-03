70f6351a6e345fad2ddca5d63f370ade7a212333
coding4vinayak <baoxing100@ip-10-192-12-235.ec2.internal>
2024-06-03 09:51:16 +0000

    ok

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello, Lightning World!')
+
+
+from flask import Flask, render_template
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Define a route for the root path ('/')
+@app.route('/')
+def index():
+  """Render the index.html template"""
+  # Pass any variables you want to access in the template here (optional)
+  return render_template("index.html")
+
+# Run the development server (only if script is run directly)
+if __name__ == '__main__':
+  app.run(debug=True)
